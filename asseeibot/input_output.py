39442e0365ce31abbe5ccec938b99e08ec0828cb
Egils Consulting <egils@riseup.net>
2021-04-05 16:35:14 +0200

    Add functions for uploading scientific authors and articles Add input_output module

---
+++ asseeibot/input_output.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+from os import path 
+from datetime import datetime, timezone
+import json
+import logging
+
+from typing import List, Union, Dict
+
+import config
+import loglevel
+
+# Logging
+logger = logging.getLogger(__name__)
+if config.loglevel is None:
+    # Set loglevel
+    logger.debug("Setting loglevel in config")
+    loglevel.set_loglevel()
+logger.setLevel(config.loglevel)
+logger.level = logger.getEffectiveLevel()
+file_handler = logging.FileHandler("input_output.log")
+logger.addHandler(file_handler)
+
+def get_wikipedia_list():
+    filename = config.wikipedia_list
+    if path.isfile(filename):
+        # Read the file
+        with open(filename, 'r', encoding='utf-8') as myfile:
+            string = myfile.read()
+            if len(string) > 0:
+                return json.loads(string)
+
+
+def save_to_wikipedia_list(
+        dois: List[str] = None,
+        language_code: str = None,
+        article: str = None,
+        done: bool = False,
+):
+    """Only has side-effects."""
+    filename = config.wikipedia_list
+    if dois is None:
+        logger.error("Error. DOIs was None")
+    else:
+        print(f"Adding {dois} to local list of DOIs found in Wikipedia")
+        for doi in dois:
+            data = dict(
+                article=article,
+                date=datetime.now().isoformat(),
+                lang=language_code,
+                done=done,
+            )
+            if path.isfile(filename):
+                # Read the file
+                with open(filename, 'r', encoding='utf-8') as myfile:
+                    json_data = myfile.read()
+                if len(json_data) > 0:
+                    with open(filename, 'w', encoding='utf-8') as myfile:
+                        # parse file
+                        wikipedia_list = json.loads(json_data)
+                        wikipedia_list[doi] = data
+                        print(wikipedia_list)
+                        if config.debug_wikipedia_list:
+                            logging.debug(f"dumping altered list:{wikipedia_list}")
+                        json.dump(wikipedia_list, myfile, ensure_ascii=False)
+                else:
+                    logger.error("Error. JSON data had no entries. Remove the"+
+                                 f" {filename} and try again.")
+            else:
+                # Create the file
+                with open(filename, "w", encoding='utf-8') as outfile:
+                    # Create new file with dict and item
+                    wikipedia_list = {}
+                    wikipedia_list[doi] = data
+                    print(wikipedia_list)
+                    if config.debug_wikipedia_list:
+                        logging.debug(f"dumping:{wikipedia_list}")
+                    json.dump(wikipedia_list, outfile, ensure_ascii=False) 
--- asseeibot/wikidata.py
+++ asseeibot/wikidata.py
@@ -67,3 +67,256 @@ def lookup_issn(issn: List[str]) -> Union[str,None]:
     else:
         print("ISSN not found on WD.")
         return None
+
+def add_scientific_author(
+        author_orcid=None,
+        given_name: str = None,
+        family_name: str = None,
+        gender: str = None,
+        language_code: str = None
+        ):
+    """Add s missing author with ORCID to WD"""
+    if given_name is None or family_name is None:
+        logger.error("Error one of the names is None")
+    label = given_name + family_name
+    description = "author of a scientific article"
+    # Claims
+    name_in_native_language = wbi_core.
+    given_name_claim = wbi_core.MonolingualText(
+        given_name,
+        735,
+        language=language_code,
+        # Add qualifiers
+        qualifiers=[],
+        # Add reference
+        references=[],
+    )
+    orcid = 
+    item = wbi_core.Item(
+        given_name_claim,
+        
+        
+    )
+    pass
+
+def add_scientific_article(
+        doi=None,
+        authors_orcid=None,
+        lid=None,
+        form_id=None,
+        sense_id=None,
+        word=None,
+        publication_date=None,
+        language_style=None,
+        type_of_reference=None,
+        source=None,
+        line=None,
+):
+    # Use WikibaseIntegrator aka wbi to upload the changes in one edit
+    link_to_form = wbi_core.Form(
+        prop_nr="P5830",
+        value=form_id,
+        is_qualifier=True
+    )
+    link_to_sense = wbi_core.Sense(
+        prop_nr="P6072",
+        value=sense_id,
+        is_qualifier=True
+    )
+    if language_style == "formal":
+        style = "Q104597585"
+    else:
+        if language_style == "informal":
+            style = "Q901711"
+        else:
+            print(_( "Error. Language style {} ".format(language_style) +
+                     "not one of (formal,informal). Please report a bug at "+
+                     "https://github.com/egils-consulting/LexUtils/issues" ))
+            sleep(config.sleep_time)
+            return "error"
+    logging.debug("Generating qualifier language_style " +
+                  f"with {style}")
+    language_style_qualifier = wbi_core.ItemID(
+        prop_nr="P6191",
+        value=style,
+        is_qualifier=True
+    )
+    # oral or written
+    if type_of_reference == "written":
+        medium = "Q47461344"
+    else:
+        if type_of_reference == "oral":
+            medium = "Q52946"
+        else:
+            print(_( "Error. Type of reference {} ".format(type_of_reference) +
+                     "not one of (written,oral). Please report a bug at "+
+                     "https://github.com/egils-consulting/LexUtils/issues" ))
+            sleep(config.sleep_time)
+            return "error"
+    logging.debug(_( "Generating qualifier type of reference " +
+                  "with {}".format(medium) ))
+    type_of_reference_qualifier = wbi_core.ItemID(
+        prop_nr="P3865",
+        value=medium,
+        is_qualifier=True
+    )
+    if source == "riksdagen":
+        if publication_date is not None:
+            publication_date = datetime.fromisoformat(publication_date)
+        else:
+            print(_("Publication date of document {} ".format(document_id) +
+                    "is missing. We have no fallback for that at the moment. " +
+                    "Abort adding usage example."))
+            return "error"
+        stated_in = wbi_core.ItemID(
+            prop_nr="P248",
+            value="Q21592569",
+            is_reference=True
+        )
+        document_id = wbi_core.ExternalID(
+            prop_nr="P8433",  # Riksdagen Document ID
+            value=document_id,
+            is_reference=True
+        )
+        reference = [
+            stated_in,
+            document_id,
+            wbi_core.Time(
+                prop_nr="P813",  # Fetched today
+                time=datetime.utcnow().replace(
+                    tzinfo=timezone.utc
+                ).replace(
+                    hour=0,
+                    minute=0,
+                    second=0,
+                ).strftime("+%Y-%m-%dT%H:%M:%SZ"),
+                is_reference=True,
+            ),
+            wbi_core.Time(
+                prop_nr="P577",  # Publication date
+                time=publication_date.strftime("+%Y-%m-%dT00:00:00Z"),
+                is_reference=True,
+            ),
+            type_of_reference_qualifier,
+        ]
+    if source == "europarl":
+        stated_in = wbi_core.ItemID(
+            prop_nr="P248",
+            value="Q5412081",
+            is_reference=True
+        )
+        reference = [
+            stated_in,
+            wbi_core.Time(
+                prop_nr="P813",  # Fetched today
+                time=datetime.utcnow().replace(
+                    tzinfo=timezone.utc
+                ).replace(
+                    hour=0,
+                    minute=0,
+                    second=0,
+                ).strftime("+%Y-%m-%dT%H:%M:%SZ"),
+                is_reference=True,
+            ),
+            wbi_core.Time(
+                prop_nr="P577",  # Publication date
+                time="+2012-05-12T00:00:00Z",
+                is_reference=True,
+            ),
+            wbi_core.Url(
+                prop_nr="P854",  # reference url
+                value="http://www.statmt.org/europarl/v7/sv-en.tgz",
+                is_reference=True,
+            ),
+            # filename in archive
+            wbi_core.String(
+                (f"europarl-v7.{config.language_code}" +
+                 f"-en.{config.language_code}"),
+                "P7793",
+                is_reference=True,
+            ),
+            # line number
+            wbi_core.String(
+                str(line),
+                "P7421",
+                is_reference=True,
+            ),
+            type_of_reference_qualifier,
+        ]
+    if source == "ksamsok":
+        # No date is provided unfortunately, so we set it to unknown value
+        stated_in = wbi_core.ItemID(
+            prop_nr="P248",
+            value="Q7654799",
+            is_reference=True
+        )
+        document_id = wbi_core.ExternalID(
+            # K-Samsök URI
+            prop_nr="P1260",  
+            value=document_id,
+            is_reference=True
+        )
+        reference = [
+            stated_in,
+            document_id,
+            wbi_core.Time(
+                prop_nr="P813",  # Fetched today
+                time=datetime.utcnow().replace(
+                    tzinfo=timezone.utc
+                ).replace(
+                    hour=0,
+                    minute=0,
+                    second=0,
+                ).strftime("+%Y-%m-%dT%H:%M:%SZ"),
+                is_reference=True,
+            ),
+            wbi_core.Time(
+                # We don't know the value of the publication dates unfortunately
+                prop_nr="P577",  # Publication date
+                time="",
+                snak_type="somevalue",
+                is_reference=True,
+            ),
+            type_of_reference_qualifier,
+        ]
+    if reference is None:
+        logger.error(_( "No reference defined, cannot add usage example" ))
+        exit(1)
+    # This is the usage example statement
+    claim = wbi_core.MonolingualText(
+        sentence,
+        "P5831",
+        language=config.language_code,
+        # Add qualifiers
+        qualifiers=[
+            link_to_form,
+            link_to_sense,
+            language_style_qualifier,
+        ],
+        # Add reference
+        references=[reference],
+    )
+    if config.debug_json:
+        logging.debug(f"claim:{claim.get_json_representation()}")
+    item = wbi_core.ItemEngine(
+        data=[claim], append_value=["P5831"], item_id=lid,
+    )
+    # if config.debug_json:
+    #     print(item.get_json_representation())
+    if config.login_instance is None:
+        # Authenticate with WikibaseIntegrator
+        print("Logging in with Wikibase Integrator")
+        config.login_instance = wbi_login.Login(
+            user=config.username, pwd=config.password
+        )
+    result = item.write(
+        config.login_instance,
+        edit_summary=(
+            _( "Added usage example "+
+               "with [[Wikidata:Tools/LexUtils]] v{}".format(config.version) )
+        )
+    )
+    if config.debug_json:
+        logging.debug(f"result from WBI:{result}")
+    # TODO add handling of result from WBI and return True == Success or False 
+    return result
