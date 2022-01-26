6b1046f5f46400e773cd6b4543b1f8abe910ac6f
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-26 14:09:48 +0100

    main.py: Switch to pywikibot and comment out old search_doi() function that did not work properly.

--- asseeibot/main.py
+++ asseeibot/main.py
@@ -2,20 +2,17 @@
 import asyncio
 import json
 import logging
-import re
-from pprint import pprint
-from typing import List, Optional
-from urllib.parse import quote, unquote
+from urllib.parse import quote
 
+import pywikibot
 from aiosseclient import aiosseclient  # type: ignore
-from mediawikiapi import MediaWikiAPI, PageError  # type: ignore
 from rich import print
 
 import config
 import input_output
 import wikidata
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
 
 # wikidata.lookup_issn("2535-7492")
 # exit(0)
@@ -27,112 +24,6 @@ doi_prefixes = ["https://doi.org/", "http://doi.org/",
 excluded_wikis = ["ceb", "zh", "ja"]
 wd_prefix = "http://www.wikidata.org/entity/"
 trust_url_file_endings = True
-
-
-# debug
-# input_output.save_to_wikipedia_list(["doi"], "en", "title")
-# exit(0)
-
-def search_isbn(page):
-    content = page.content
-    # TODO search for isbns
-    #             found = True
-    #             isbns.append(isbn)
-    #             print(
-    #                 f"{found_text}{doi}",
-    #             )
-    #     if found:
-    #         lookup_isbn(isbns)
-    #         sleep(1)
-    # else:
-    #     print("ISBN number not found")
-
-
-def search_doi(page) -> Optional[List[str]]:
-    logger = logging.getLogger(__name__)
-    links = page.references
-    if links is not None:
-        logger.debug(f"References:{links}")
-        found = False
-        dois = set()
-        # This should catch >99% of all DOIs
-        # but it seems it does not...
-        # doi_regex_pattern = "/^10.\d{4,9}/[-._;()/:A-Z0-9]+$/i"
-        # This is my simplified pattern
-        doi_regex_pattern = "10.\d{4,9}\/+.+"
-        for link in links:
-            # We unquote to avoid the URL garbage like %A1
-            match = re.search(doi_regex_pattern, unquote(link))
-            if match is not None:
-                found_excluded_word = False
-                doi = match.group()
-                excluded_words = ["http", ".pdf", "jsessionid", "htm", "jpg", "png"]
-                for word in excluded_words:
-                    if word in doi:
-                        found_excluded_word = True
-                        # This is probably a fulltext url, so we skip it
-                        # e.g. 10.1525/aa.1965.67.5.02a00560/asset/aa.1965.67.5.02a00560.pdf;jsessionid=F9912E4EA20516C07E35701700EEBE71.f04t01?v=1&t=hoovxr8v&s=91fed24eb577
-                        # 9c1f45a2e1729d1dc3e0bf734fab
-                if not found_excluded_word:
-                    found = True
-                    dois.add(doi)
-                    logger.info(f"found DOI via regex: {doi}")
-            for doi_prefix in doi_prefixes:
-                if link.find(doi_prefix) != -1:
-                    # unquote to convert %2F -> /
-                    doi = unquote(link.replace(doi_prefix, ''))
-                    if doi not in dois:
-                        found = True
-                        dois.add(doi)
-                        found_text = "[bold red]DOI link found via prefix, but not the regex:[/bold red] "
-                        print(f"{found_text}{doi}")
-                        print(match)
-                        exit()
-            if link.find("doi") != -1:
-                known_prefix = False
-                for doi_prefix in doi_prefixes:
-                    if link.find(doi_prefix) != -1:
-                        known_prefix = True
-                if not known_prefix:
-                    print(f"new doi prefix found: {link}")
-                    #exit()
-        if found:
-            logger.debug(f"found the following dois")
-            pprint(dois)
-            #exit()
-            return dois
-        else:
-            print("External links not found")
-            return None
-    else:
-        print("External links not found")
-        return None
-
-
-def download_page(
-        mediawikiapi,
-        language_code: str = None,
-        title: str = None,
-):
-    logger = logging.getLogger(__name__)
-    logger.info("Downloading wikitext")
-    mediawikiapi.config.language = language_code
-    # print(mediawikiapi.summary(title, sentences=1))
-    page = False
-    try:
-        page = mediawikiapi.page(title)
-        success = True
-        if page is not None:
-            logger.info("Download finished")
-            return page
-        else:
-            return None
-    except PageError:
-        logger.warning("Got page error, skipping")
-        success = False
-        return None
-    # print(page.sections)
-    # print("Looking for templates")
 
 
 def check_if_done(doi):
@@ -148,7 +39,6 @@ def check_if_done(doi):
 
 def finish_all_in_list():
     print("Going through the local list of found DOIs and finishing importing them all")
-    data = None
     data = input_output.get_wikipedia_list()
     dois = []
     if data is not None:
@@ -162,51 +52,71 @@ def finish_all_in_list():
 
 
 def process_event(
-        mediawikiapi,
+        site,
         language_code=None,
         title=None,
 ):
-    page = download_page(
-        mediawikiapi,
-        language_code=language_code,
-        title=title,
-    )
-    dois = None
-    missing_dois = None
-    if page is not None:
-        dois = search_doi(page)
+    logger = logging.getLogger(__name__)
+    # TODO flesh out the finding of cite journal and
+    #  create an OOP model for it
+    page = pywikibot.Page(site, title)
+    raw = page.raw_extracted_templates
+    dois = set()
+    for template_name, content in raw:
+        # print(f"working on {template_name}")
+        if template_name.lower() == "cite journal":
+            print(f"content:{content}")
+            journal = None
+            doi = None
+            jstor = None
+            pmid = None
+            article_title = None
+            for key, value in content.items():
+                if key == "doi":
+                    logger.info(f"Found doi: {value}")
+                    doi = value
+                    dois.add(doi)
+                if key == "journal":
+                    logger.info(f"Found journal: {value}")
+                    journal = value
+                if key == "jstor":
+                    logger.info(f"Found jstor: {value}")
+                    jstor = value
+                if key == "pmid":
+                    logger.info(f"Found pmid: {value}")
+                    pmid = value
+                if key == "title":
+                    logger.info(f"Found title: {value}")
+                    article_title = value
+            if doi is None:
+                logger.warning(f"An article titled {article_title} in the journal {journal} was found but no DOI. "
+                               f"pmid:{pmid} jstor:{jstor}")
+    missing_dois = []
+    if config.import_mode:
+        # Remove the DOIs that are marked done locally
         if dois is not None and len(dois) > 0:
             for doi in dois:
                 done = check_if_done(doi)
                 if done:
                     dois.remove(doi)
-                    print(f"{doi} has been found before.")
-        if dois is not None and not done:
-            # input('Press enter to continue: ')
-            missing_dois = wikidata.lookup_dois(dois=dois, in_wikipedia=True)
-            if len(missing_dois) > 0:
-                input_output.save_to_wikipedia_list(missing_dois, language_code, title)
-            # if config.import_mode:
-            # answer = util.yes_no_question(
-            #     f"{doi} is missing in WD. Do you"+
-            #     " want to add it now?"
-            # )
-            # if answer:
-            #     crossref.lookup_data(doi=doi, in_wikipedia=True)
-            #     pass
-            # else:
-            #     pass
-        else:
-            pass
+                    logger.info(f"{doi} has been found before.")
+    if len(dois) > 0:
+        input('Press enter to continue: ')
+        missing_dois = wikidata.lookup_dois(dois=dois, in_wikipedia=True)
+        if missing_dois is not None and len(missing_dois) > 0:
+            input_output.save_to_wikipedia_list(missing_dois, language_code, title)
+        # if config.import_mode:
+        # answer = util.yes_no_question(
+        #     f"{doi} is missing in WD. Do you"+
+        #     " want to add it now?"
+        # )
+        # if answer:
+        #     crossref.lookup_data(doi=doi, in_wikipedia=True)
+        #     pass
+        # else:
+        #     pass
     # Return tuple with counts
-    if dois is not None and missing_dois is None:
-        return (len(dois), 0)
-    elif dois is None and missing_dois is not None:
-        return (len(dois), len(missing_dois))
-    elif dois is not None and missing_dois is not None:
-        return (len(dois), len(missing_dois))
-    else:
-        return (0, 0)
+    return (len(dois), len(missing_dois))
 
 
 async def main():
@@ -214,7 +124,8 @@ async def main():
     if config.import_mode:
         finish_all_in_list()
     print("Looking for new DOIs from the Event stream")
-    mediawikiapi = MediaWikiAPI()
+    # We support only the English Wikipedia for now
+    site = pywikibot.Site('en', 'wikipedia')
     count = 0
     count_dois_found = 0
     count_missing_dois = 0
@@ -250,8 +161,8 @@ async def main():
                 logger.info(f"{type}\t{server_name}\t{bot}\t\"{title}\"")
                 logger.info(f"http://{server_name}/wiki/{quote(title)}")
                 dois_count_tuple = process_event(
-                    mediawikiapi,
-                    language_code=language_code,
+                    site,
+                    # language_code=language_code,
                     title=title,
                 )
                 if dois_count_tuple[0] > 0:
@@ -267,3 +178,109 @@ async def main():
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
+
+# debug
+# input_output.save_to_wikipedia_list(["doi"], "en", "title")
+# exit(0)
+
+# def search_isbn(page):
+#     content = page.content
+#     # TODO search for isbns
+#     #             found = True
+#     #             isbns.append(isbn)
+#     #             print(
+#     #                 f"{found_text}{doi}",
+#     #             )
+#     #     if found:
+#     #         lookup_isbn(isbns)
+#     #         sleep(1)
+#     # else:
+#     #     print("ISBN number not found")
+
+
+# def old_search_doi(page) -> Optional[List[str]]:
+#     logger = logging.getLogger(__name__)
+#     links = page.references
+#     if links is not None:
+#         logger.debug(f"References:{links}")
+#         found = False
+#         dois = set()
+#         # This should catch >99% of all DOIs
+#         # but it seems it does not...
+#         # doi_regex_pattern = "/^10.\d{4,9}/[-._;()/:A-Z0-9]+$/i"
+#         # This is my simplified pattern
+#         doi_regex_pattern = "10.\d{4,9}\/+.+"
+#         for link in links:
+#             # We unquote to avoid the URL garbage like %A1
+#             match = re.search(doi_regex_pattern, unquote(link))
+#             if match is not None:
+#                 found_excluded_word = False
+#                 doi = match.group()
+#                 excluded_words = [
+#                     "/ww-",  # whoswho
+#                     "http", ".pdf", "jsessionid", "htm", "jpg", "png"
+#                 ]
+#                 for word in excluded_words:
+#                     if word in doi:
+#                         found_excluded_word = True
+#                         # This is probably a fulltext url, so we skip it
+#                         # e.g. 10.1525/aa.1965.67.5.02a00560/asset/aa.1965.67.5.02a00560.pdf;jsessionid=F9912E4EA20516C07E35701700EEBE71.f04t01?v=1&t=hoovxr8v&s=91fed24eb577
+#                         # 9c1f45a2e1729d1dc3e0bf734fab
+#                 if not found_excluded_word:
+#                     found = True
+#                     dois.add(doi)
+#                     logger.info(f"found DOI via regex: {doi}")
+#             for doi_prefix in doi_prefixes:
+#                 if link.find(doi_prefix) != -1:
+#                     # unquote to convert %2F -> /
+#                     doi = unquote(link.replace(doi_prefix, ''))
+#                     if doi not in dois:
+#                         found = True
+#                         dois.add(doi)
+#                         found_text = "[bold red]DOI link found via prefix, but not the regex:[/bold red] "
+#                         print(f"{found_text}{doi}")
+#                         print(match)
+#                         exit()
+#             if link.find("doi") != -1:
+#                 known_prefix = False
+#                 for doi_prefix in doi_prefixes:
+#                     if link.find(doi_prefix) != -1:
+#                         known_prefix = True
+#                 if not known_prefix:
+#                     print(f"new doi prefix found: {link}")
+#                     # exit()
+#         if found:
+#             logger.debug(f"found the following dois")
+#             pprint(dois)
+#             # exit()
+#             return dois
+#         else:
+#             print("External links not found")
+#             return None
+#     else:
+#         print("External links not found")
+#         return None
+
+
+# def download_page(
+#         mediawikiapi: MediaWikiAPI,
+#         language_code: str = None,
+#         title: str = None,
+# ):
+#     logger = logging.getLogger(__name__)
+#     logger.info("Downloading wikitext")
+#     mediawikiapi.config.language = language_code
+#     # print(mediawikiapi.summary(title, sentences=1))
+#     page = False
+#     try:
+#         page = mediawikiapi.page(title=title, auto_suggest=False)
+#         if page is not None:
+#             logger.info("Download finished")
+#             return page
+#         else:
+#             return None
+#     except PageError:
+#         logger.warning("Got page error, skipping")
+#         return None
+#     # print(page.sections)
+#     # print("Looking for templates")
