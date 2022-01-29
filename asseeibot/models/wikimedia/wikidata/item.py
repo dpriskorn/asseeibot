2ba30c7363a4aa03eb6acd6eee712a7eb13ed1f4
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-29 01:12:38 +0100

    item.py: New method __call_wbi_search_entities__()

--- asseeibot/models/wikimedia/wikidata/item.py
+++ asseeibot/models/wikimedia/wikidata/item.py
@@ -1,9 +1,17 @@
 from urllib.parse import quote
 
 from pydantic import BaseModel
+from wikibaseintegrator.wbi_helpers import search_entities
 
 
 class Item(BaseModel):
+
+    @staticmethod
+    def __call_wbi_search_entities__(subject):
+        return search_entities(search_string=subject,
+                               language="en",
+                               dict_result=True,
+                               max_results=1)
 
     @staticmethod
     def string_search_url(string: str) -> str:
