1784b0816dde36b6913e2b2efced21834dd48e0b
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-29 15:11:21 +0100

    Support getting aliases and description via WBI

--- asseeibot/models/wikimedia/wikidata/item.py
+++ asseeibot/models/wikimedia/wikidata/item.py
@@ -1,10 +1,42 @@
+from typing import Optional, List
 from urllib.parse import quote
 
 from pydantic import BaseModel
+from wikibaseintegrator import wbi_config, WikibaseIntegrator
+from wikibaseintegrator.entities import Item as EntityItem
 from wikibaseintegrator.wbi_helpers import search_entities
+
+import config
+from asseeibot.models.wikimedia.enums import WikimediaLanguage
 
 
 class Item(BaseModel):
+    qid: str
+    __item: Optional[EntityItem]
+    __aliases: Optional[List[str]]
+    __description: Optional[str]
+
+    def __fetch__(self):
+        # fetch using WBI
+        wbi_config.config["USER_AGENT_DEFAULT"] = config.user_agent
+        wbi = WikibaseIntegrator(login=None)
+        self.__item = wbi.item.get()
+
+    @property
+    def aliases(self):
+        if self.__item is None:
+            self.__fetch__()
+        if self.__aliases is None:
+            self.__aliases = self.__item.aliases.get(WikimediaLanguage.ENGLISH.value)
+        return self.__aliases
+
+    @property
+    def description(self):
+        if self.__item is None:
+            self.__fetch__()
+        if self.__description is None:
+            self.__description = self.__item.descriptions.get(WikimediaLanguage.ENGLISH.value)
+        return self.__description
 
     @staticmethod
     def __call_wbi_search_entities__(subject):
