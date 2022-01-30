d6096f5c049160a975113f17f3b7ddd5ce97dbcb
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-30 11:55:17 +0100

    Implement argparse and new flag --delete-match which enable the user to delete a match. fixes #10 argparse_setup.py: New file __init__.py: main(): Use argparse delete_match(): New function

---
+++ asseeibot/helpers/argparse_setup.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def setup_argparse_and_return_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="""
+Asseeibot enables working main subject statements on items based on a 
+heuristic matching the subjects on works in Crossref with a custom domain specific ontology. 
+
+It caches all matches for later reuse.
+
+You can delete a match from the cache if you accidentally made a mistake or later regret your decision.
+
+NOTE: we do not yet support removing statements from Wikidata based on the bad match.
+    """)
+    parser.add_argument(
+        '--delete-match',
+        nargs='?',
+        help="Delete a match from your local cache of earlier matches."
+    )
+    return parser.parse_args()
