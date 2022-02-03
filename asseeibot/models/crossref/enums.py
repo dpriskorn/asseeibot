7eea212bcbb94993702585ea719c39975fa7af40
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-03 13:43:57 +0100

    New CrossrefEntryType value

--- asseeibot/models/crossref/enums.py
+++ asseeibot/models/crossref/enums.py
@@ -3,10 +3,11 @@ from enum import Enum
 
 class CrossrefEntryType(Enum):
     BOOK = "book"
-    JOURNAL_ARTICLE = "journal-article"
-    PROCEEDINGS_ARTICLE = "proceedings-article"
     BOOK_CHAPTER = "book-chapter"
     COMPONENT = "component"
+    JOURNAL_ARTICLE = "journal-article"
+    PROCEEDINGS_ARTICLE = "proceedings-article"
+    REFERENCE_ENTRY = "reference-entry"
 
 
 class CrossrefContentType(Enum):
